274f48f662482b46d2ff97bcfbd837ce448f9005
LucianHitklif <asmetkina.va10@gmail.com>
2026-04-06 04:12:19 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,1 +1,8 @@
-
+log1 = input().split()
+log2 = input().split()
+log3 = input().split()
+set1, set2, set3 = set(log1), set(log2), set(log3)
+all_ips = set1 | set2 | set3
+common_all = set1 & set2 & set3
+result = sorted(all_ips - common_all)
+print(' '.join(result))
